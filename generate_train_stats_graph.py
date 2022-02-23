06962825cf4e2fdefdf6286383b6eef9d3d8b3d5
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2022-02-23 15:14:58 +0000

    Refactor visualisation scripts to be more modular, eaiser to read, and compatible with the new distributed training pipeline.

--- generate_train_stats_graph.py
+++ generate_train_stats_graph.py
@@ -3,17 +3,25 @@ generate_train_stats_graph.py
 purpose: Executable script to generate a graph visualising the training
          statistics for a particular version of the CDR3 BERT model.
 author: Yuta Nagano
-ver: 2.0.0
+ver: 3.0.0
 '''
 
 
 import argparse
+import matplotlib
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
+import re
 
 
-if __name__ == '__main__':
+def parse_command_line_arguments() -> str:
+    '''
+    Parse command line arguments from stdin (the ID of the training run in
+    question) and return that as a string. Before returning, check that there is
+    a local folder which corresponds to the specified run ID, and if not, raise
+    an error.
+    '''
     parser = argparse.ArgumentParser(
         description='Generate a graph visualising the training statistics ' + \
             'for a particular version of the CDR3 BERT model.'
@@ -25,14 +33,59 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    RUN_ID = args.run_id
+    if not os.path.isdir(os.path.join('training_runs',args.run_id)):
+        raise RuntimeError(
+            f'No local directory (training_runs/{args.run_id}) could be found '\
+            'which matches the run ID specified.'
+        )
 
-    stats = pd.read_csv(os.path.join('training_runs',RUN_ID,'train_stats.csv'),
-                        index_col='epoch')
+    return args.run_id
 
+
+def load_training_stats(run_id: str) -> list:
+    '''
+    Given a run ID, load all training stats csvs available from that run using
+    pandas, and return them all in a list.
+    '''
+    path_to_directory = os.path.join('training_runs',run_id)
+
+    # Get a list of the contents of the train run directory
+    contents = os.listdir(path_to_directory)
+
+    # Only keep files that look like train_stats csvs
+    train_stats = [os.path.join(path_to_directory, path) for path in contents \
+        if re.fullmatch('^train_stats.*\.csv$', path)]
+    
+    # Raise error if no matches
+    if len(train_stats) == 0:
+        raise RuntimeError(
+            f'No train stats csvs could be found for training run id {run_id}.'
+        )
+    
+    # Load each identified train stat csv using pandas
+    train_stat_dfs = [
+        pd.read_csv(path,index_col='epoch') for path in train_stats
+    ]
+
+    return train_stat_dfs
+
+
+def draw_figure(train_stat_dfs: list) -> matplotlib.figure.Figure:
+    '''
+    Given a set of pandas dataframes containing training statistics, draw a
+    matplotlib figure that summarises the data, and return the figure object.
+    '''
+    # Take note of the number of epochs worth of data there is
+    # The '-1' is because the last row of every dataframe is for jumbled loss (
+    # not part of the training loop)
+    epochs = len(train_stat_dfs[0]) - 1
+
+    # Create figure
     fig = plt.figure(figsize=(8,8))
 
+    # Create top panel (Loss)
     loss = plt.subplot(5,1,(1,2))
+    loss.set_xticks(ticks=range(1,epochs + 1))
     loss.tick_params(
         axis='x',
         bottom=False,
@@ -40,11 +93,9 @@ if __name__ == '__main__':
     )
     loss.set_title('Loss')
 
-    loss.plot(stats['train_loss'],label='training loss')
-    loss.plot(stats['valid_loss'],label='validation loss')
-    loss.legend(loc='upper right')
-
+    # Create middle panel (Accuracy)
     acc = plt.subplot(5,1,(3,4))
+    acc.set_xticks(ticks=range(1,epochs + 1))
     acc.tick_params(
         axis='x',
         bottom=False,
@@ -52,18 +103,56 @@ if __name__ == '__main__':
     )
     acc.set_title('Accuracy')
 
-    acc.plot(stats['train_acc'],label='training accuracy')
-    acc.plot(stats['valid_acc'],label='validation accuracy')
-    acc.legend(loc='lower right')
-
+    # Create bottom panel (Learning rate)
     lr = plt.subplot(5,1,5)
-    lr.set_xticks(
-        ticks=range(1,21)
-    )
+    lr.set_xticks(ticks=range(1,epochs + 1))
     lr.set_title('Average Learning Rate')
 
-    lr.plot(stats['avg_lr'],c='g')
+    # Plot top and middle panels (Loss and Accuracy)
+    for df in train_stat_dfs:
+        # Plot top panel (Loss)
+        loss.plot(df['train_loss'],c='tab:blue')
+        loss.plot(df['valid_loss'],c='tab:orange')
 
+        # Plot middle panel (Accuracy)
+        acc.plot(df['train_acc'],c='tab:blue')
+        acc.plot(df['valid_acc'],c='tab:orange')
+
+    # Plot bottom panel (Learning rate)
+    lr.plot(train_stat_dfs[0]['avg_lr'],c='tab:green')
+
+    # Create legends
+    loss.legend(('training loss','validation loss'),loc='upper right')
+    acc.legend(('training accuracy','validation accuracy'),loc='lower right')
+
+    # Clean up figure
     fig.tight_layout()
 
-    plt.savefig(os.path.join('training_runs',RUN_ID,'train_stats.png'))
+    return fig
+
+
+def main(run_id: str) -> None:
+    '''
+    Given a particular run ID, search for a local directory corresponding to
+    that training run, fetch training stats data from that directory, produce
+    a visualisation of the training statistics, and save the produced figure.
+    '''
+    # Read in the training stats
+    stats = load_training_stats(run_id)
+
+    # Create the figure
+    fig = draw_figure(stats)
+
+    # Save figure
+    fig.savefig(os.path.join('training_runs',run_id,'train_stats.png'))
+
+
+if __name__ == '__main__':
+    # Set matplotlib stylesheet
+    plt.style.use('seaborn')
+
+    # Get run ID from the command line
+    run_id = parse_command_line_arguments()
+    
+    # Run the main function
+    main(run_id)
--- trained_bert_demo.py
+++ trained_bert_demo.py
@@ -4,22 +4,32 @@ purpose: Executable script to generate demo visualisations of a specified
          trained DR3 BERT model doing masked-residue modelling on a random
          selection of CDR3s from the testing set.
 author: Yuta Nagano
-ver: 2.0.0
+ver: 3.0.0
 '''
 
 
+import argparse
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import os
 import pandas as pd
 import random
-import argparse
 import torch
 from torch.nn import functional as F
+
 from source.data_handling import tokenise, lookup
 
+from hyperparams import hyperparams
 
-if __name__ == '__main__':
+
+def parse_command_line_arguments() -> str:
+    '''
+    Parse command line arguments from stdin (the ID of the training run in
+    question) and return that as a string. Before returning, check that there is
+    a local folder which corresponds to the specified run ID, and if not, raise
+    an error.
+    '''
     parser = argparse.ArgumentParser(
         description='Generate demo visualisations of a specified trained ' + \
             'CDR3 BERT model doing masked-residue modelling on a random ' + \
@@ -32,72 +42,128 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
+    if not os.path.isdir(os.path.join('training_runs',args.run_id)):
+        raise RuntimeError(
+            f'No local directory (training_runs/{args.run_id}) could be found '\
+            'which matches the run ID specified.'
+        )
 
-    RUN_ID = args.run_id
-
-
-    random.seed(42)
-
-
-    model = torch.load(os.path.join('training_runs',RUN_ID,'trained_model.ptnn'))
-    ds = pd.read_csv('data/test.csv')
+    return args.run_id
 
 
+@torch.no_grad()
+def test_at_index(
+    model: torch.nn.Module,
+    cdr3: str,
+    index: int,
+    k: int = 5
+) -> torch.tensor:
+    # Ensure that the model is in evaluation mode
+    model.eval()
+
+    # Mask the residue at index
+    masked = list(cdr3)
+    masked[index] = '?'
+
+    # Tokenise the cdr3
+    tokenised = tokenise(masked)
+    tokenised = tokenised.unsqueeze(0)
+
+    mask = torch.zeros(tokenised.size())
+
+    # Run the masked sequence through the model
+    out = model(tokenised, mask)
+
+    # Get the confidence distribution for the masked token prediction
+    dist = F.softmax(out[0, index],dim=-1)
+
+    return torch.topk(dist,k,dim=-1)
+
+
+def generate_plot(
+    model: torch.nn.Module,
+    cdr3: str
+) -> matplotlib.figure.Figure:
+    '''
+    Given a compatible pytorch model and a CDR3, generate a demo plot showcasing
+    the model's understanding of the CDR3 amino acid sequence language by
+    plotting the model's top 5 estimations of which amino acids should reside at
+    each position, using the surrounding amino acid sequences as conditioning.
+    '''
+    # Generate a figure with appropriate dimensions
+    l = len(cdr3)
+    fig = plt.figure(figsize=(l,5))
+
+    # Get a gridspec to make it easier to organise panels
+    gs = GridSpec(5, l)
+
+    # For each amino acid residue in the CDR3
+    for i, aa in enumerate(cdr3):
+        # Draw that letter at an appropriate position arond the top of the fig
+        r = fig.add_subplot(gs[0,i])
+        r.axis('off')
+        r.text(0,0,aa,fontsize=48)
+
+        # Get the model's top 5 predictions of the amino acid residue at that
+        # position using the rest of the sequence as conditioning info
+        confidences, indices = test_at_index(model, cdr3, i)
+        confidences = list(reversed(confidences))
+        guesses = list(reversed([lookup(idx.item()) for idx in indices]))
+
+        # Plot a barchart of the top 5 guesses and their corresponding
+        # confidence values
+        h = fig.add_subplot(gs[1:,i])
+        bars = h.barh(range(5),confidences)
+        h.set_xlim(0,1)
+        h.set_yticks(ticks=range(5),labels=guesses)
+        h.tick_params(axis='y',which='both',length=0)
+
+        # If any of the guesses are correct, highlight them in red
+        for idx, label in enumerate(h.get_yticklabels()):
+            if label.get_text() == aa:
+                label.set_color('tab:red')
+                bars[idx].set_color('tab:red')
+
+    # Clean up the figure
+    fig.tight_layout()
+
+    return fig
+
+
+def main(run_id: str):
+    '''
+    Given a particular run ID, load the trained model from that run, and use
+    it to generate some demo plots of the model trying to guess the residues in
+    a selection of CDR3s taken from the test set.
+    '''
+
+    # Load the trained model
+    model = torch.load(
+        os.path.join('training_runs',run_id,'trained_model.ptnn')
+    )
+
+    # Load the testing set
+    ds = pd.read_csv(hyperparams['path_valid_data'])
+
+    # Get a sample of CDR3s from the testing set
     cdr3s = random.sample(ds['CDR3'].to_list(),5)
 
-
-    @torch.no_grad()
-    def test_at_index(model: torch.nn.Module, cdr3: str, index: int, k=5):
-        # Ensure that the model is in evaluation mode
-        model.eval()
-
-        # Mask the residue at index
-        masked = list(cdr3)
-        masked[index] = '?'
-
-        # Tokenise the cdr3
-        tokenised = tokenise(masked)
-        tokenised = tokenised.unsqueeze(0)
-
-        mask = torch.zeros(tokenised.size())
-
-        # Run the masked sequence through the model
-        out = model(tokenised, mask)
-
-        # Get the confidence distribution for the masked token prediction
-        dist = F.softmax(out[0, index],dim=-1)
-
-        return torch.topk(dist,k,dim=-1)
-
-        
-    def generate_plot(model: torch.nn.Module, cdr3: str):
-        l = len(cdr3)
-        fig = plt.figure(figsize=(l,5))
-        gs = GridSpec(5, l)
-
-        for i, aa in enumerate(cdr3):
-            r = fig.add_subplot(gs[0,i])
-            r.axis('off')
-            r.text(0,0,aa,fontsize=48)
-
-            confidences, indices = test_at_index(model, cdr3, i)
-            confidences = list(reversed(confidences))
-            guesses = list(reversed([lookup(idx.item()) for idx in indices]))
-
-            h = fig.add_subplot(gs[1:,i])
-            bars = h.barh(range(5),confidences)
-            h.set_xlim(0,1)
-            h.set_yticks(ticks=range(5),labels=guesses)
-            h.tick_params(axis='y',which='both',length=0)
-            for idx, label in enumerate(h.get_yticklabels()):
-                if label.get_text() == aa:
-                    label.set_color('tab:red')
-                    bars[idx].set_color('tab:red')
-
-        fig.tight_layout()
-        
-        plt.savefig(os.path.join('training_runs',RUN_ID,f'{cdr3}.png'))
-
-
+    # Generate a demo plot for each CDR3
     for cdr3 in cdr3s:
-        generate_plot(model, cdr3)
+        fig = generate_plot(model, cdr3)
+
+        # Save the generated figure
+        fig.savefig(
+            os.path.join('training_runs',run_id,f'{cdr3}.png')
+        )
+
+
+if __name__ == '__main__':
+    # Set the random seed
+    random.seed(42)
+
+    # Parse command line arguments
+    run_id = parse_command_line_arguments()
+
+    # Execute main function
+    main(run_id)
