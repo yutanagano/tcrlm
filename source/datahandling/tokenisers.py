4f55d744b7620c7ac05c3f5abbc196f360d1317c
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2022-09-07 17:26:02 +0100

    Implement new modular tokeniser class which can handle overlapping amino acid tuplet tokens.

--- source/datahandling/tokenisers.py
+++ source/datahandling/tokenisers.py
@@ -1,8 +1,24 @@
-'Custom tokeniser classes for amino acid tokenisation.'
+'''
+Custom tokeniser classes for amino acid tokenisation.
+
+Tokenisers should understand the following characters:
+
+The mask character: ?
+The amino acids:    ACDEFGHIKLMNPQRSTVWY
+
+Because of how this codebase modularises tokenisation, the vocabulary can
+change depending on the setting. To keep some consistency, there are two tokens
+that should always be mapped to the same index:
+
+- The padding token, which should always be mapped to 0
+- The 'masked' or 'unknown' token, which should always be mapped to 1
+'''
 
 
 from itertools import product
+import random
 import torch
+from typing import Iterable
 
 
 amino_acids = (
@@ -35,7 +51,7 @@ class AaTokeniser:
         tensor form.
         '''
 
-        num_tokens = len(aa_seq) - self._len_tuplet + 1
+        num_tokens = len(aa_seq) + 1 - self._len_tuplet
         tokens = []
 
         for i in range(num_tokens):
@@ -49,3 +65,111 @@ class AaTokeniser:
             tokens.append(self._token_to_int[token])
 
         return torch.tensor(tokens, dtype=torch.long, requires_grad=False)
+
+
+    def generate_mlm_pair(
+        self,
+        aa_seq: str,
+        p_mask: float,
+        p_mask_random: float,
+        p_mask_keep: float
+    ) -> tuple:
+        seq_len = len(aa_seq)
+
+        indices_to_mask = self._pick_masking_indices(
+            seq_len=seq_len,
+            p_mask=p_mask
+        )
+        masked_seq = self._generate_masked_seq(
+            seq=aa_seq,
+            indices_to_mask=indices_to_mask,
+            p_mask_random=p_mask_random,
+            p_mask_keep=p_mask_keep
+        )
+        tokenised_x = self.tokenise(aa_seq=masked_seq)
+
+        adjusted_indices = self._adjust_indices(
+            indices=indices_to_mask,
+            seq_len=seq_len
+        )
+        tokenised_seq = self.tokenise(aa_seq=aa_seq)
+        tokenised_y = self._apply_index_filter(
+            tokenised_seq=tokenised_seq,
+            indices=adjusted_indices
+        )
+
+        return (tokenised_x, tokenised_y)
+
+
+    def _pick_masking_indices(self, seq_len: int, p_mask: float) -> list:
+        if p_mask < 0 or p_mask >= 1:
+            raise RuntimeError(f'p_mask must lie in [0,1): {p_mask}')
+
+        if p_mask == 0:
+            return []
+        
+        num_to_be_masked = max(1, round(seq_len * p_mask))
+        return random.sample(range(seq_len), num_to_be_masked)
+
+
+    def _generate_masked_seq(
+        self,
+        seq: str,
+        indices_to_mask: Iterable,
+        p_mask_random: float,
+        p_mask_keep: float
+    ) -> str:
+        if p_mask_random < 0 or p_mask_random > 1:
+            raise RuntimeError(
+                f'p_mask_random must lie in [0,1]: {p_mask_random}'
+            )
+
+        if p_mask_keep < 0 or p_mask_keep > 1:
+            raise RuntimeError(
+                f'p_mask_keep must lie in [0,1]: {p_mask_keep}'
+            )
+
+        if p_mask_random + p_mask_keep > 1:
+            raise RuntimeError(
+                'p_mask_random + p_mask_keep must be less than 1.'
+            )
+
+        seq = list(seq)
+
+        for i in indices_to_mask:
+            r = random.random()
+            if r < p_mask_random:
+                # Ensure residue is replaced with a DISTINCT residue
+                seq[i] = random.sample(set(amino_acids) - {seq[i]},1)[0]
+                continue
+
+            if r < 1 - p_mask_keep:
+                seq[i] = '?'
+                continue
+        
+        return ''.join(seq)
+
+
+    def _adjust_indices(self, indices: Iterable, seq_len: int) -> set:
+        adjusted_indices = []
+
+        for i in indices:
+            adjusted = [
+                new_i for new_i in range(i+1-self._len_tuplet, i+1) \
+                if new_i >= 0 and new_i < seq_len + 1 - self._len_tuplet
+            ]
+
+            adjusted_indices.extend(adjusted)
+
+        return set(adjusted_indices)
+
+
+    def _apply_index_filter(
+        self,
+        tokenised_seq: torch.Tensor,
+        indices: Iterable
+    ) -> torch.Tensor:
+        filtered = torch.zeros_like(tokenised_seq)
+        for i in indices:
+            filtered[i] = tokenised_seq[i]
+        return filtered
--- tests/0_unit_tests/test_datahandling_tokeniser.py
+++ tests/0_unit_tests/test_datahandling_tokeniser.py
@@ -5,14 +5,6 @@ import torch
 
 class TestAaTokeniser:
     @pytest.mark.parametrize(
-        'len_tuplet', (-1, 'foo', None)
-    )
-    def test_error_if_bad_len_tuplet(self, len_tuplet):
-        with pytest.raises(Exception):
-            tokenisers.AaTokeniser(len_tuplet=len_tuplet)
-
-
-    @pytest.mark.parametrize(
         ('len_tuplet', 'expected_token_list'),
         (
             (1, [3, 2, 17, 17, 15, 21, 6]),
@@ -32,19 +24,186 @@ class TestAaTokeniser:
 
 
     @pytest.mark.parametrize(
-        ('len_tuplet', 'expected_token_list'),
+        ('len_tuplet', 'tokenised_list'),
         (
-            (1, [3, 2, 17, 1, 15, 21, 6]),
-            (2, [22, 17, 1, 1, 281, 386]),
-            (3, [417, 1, 1, 1, 5586])
+            (1, [3,2,17,21,6]),
+            (2, [22, 17, 321, 386]),
+            (3, [417, 321, 6386])
         )
     )
-    def test_tokenise_masked(self, len_tuplet, expected_token_list):
+    def test_generate_mlm_pair(self, len_tuplet, tokenised_list):
         tokeniser = tokenisers.AaTokeniser(len_tuplet=len_tuplet)
 
-        aa_seq = 'CAS?QYF'
+        aa_seq = 'CASYF'
+        tokenised = torch.tensor(tokenised_list, dtype=torch.long)
 
-        result = tokeniser.tokenise(aa_seq=aa_seq)
-        expected = torch.tensor(expected_token_list, dtype=torch.long)
+        result_x, result_y = tokeniser.generate_mlm_pair(
+            aa_seq=aa_seq,
+            p_mask=0.2,
+            p_mask_random=0,
+            p_mask_keep=0
+        )
 
-        assert result.equal(expected)
+        def reconstruct(x, y):
+            masked_indices = [i for i, t in enumerate(y) if t != 0]
+            reconstructed = x.clone()
+            for i in masked_indices:
+                reconstructed[i] = y[i]
+            return reconstructed, masked_indices
+
+        reconstructed, masked_indices = reconstruct(result_x, result_y)
+
+        assert type(result_x) == torch.Tensor
+        assert type(result_y) == torch.Tensor
+        assert len(masked_indices) in range(1,len_tuplet+1)
+        assert reconstructed.equal(tokenised)
+
+
+    @pytest.mark.parametrize(
+        ('p_mask', 'expected_num_of_masked_i'),
+        (
+            (0.8, 4),
+            (0.6, 3),
+            (0.4, 2),
+            (0.2, 1),
+            (0, 0)
+        )
+    )
+    def test_generate_mlm_pair_p_mask(self, p_mask, expected_num_of_masked_i):
+        tokeniser = tokenisers.AaTokeniser(len_tuplet=1)
+
+        aa_seq = 'CASYF'
+
+        _, result_y = tokeniser.generate_mlm_pair(
+            aa_seq=aa_seq,
+            p_mask=p_mask,
+            p_mask_random=0,
+            p_mask_keep=0
+        )
+
+        masked_indices = [i for i, t in enumerate(result_y) if t != 0]
+
+        assert len(masked_indices) == expected_num_of_masked_i
+
+
+    def test_generate_mlm_pair_mask_token(self):
+        tokeniser = tokenisers.AaTokeniser(len_tuplet=1)
+
+        aa_seq = 'CASYF'
+
+        result_x, result_y = tokeniser.generate_mlm_pair(
+            aa_seq=aa_seq,
+            p_mask=0.4,
+            p_mask_random=0,
+            p_mask_keep=0
+        )
+
+        masked_indices = [i for i, t in enumerate(result_y) if t != 0]
+
+        assert all([result_x[i] == 1 for i in masked_indices])
+
+
+    def test_generate_mlm_pair_random_token(self):
+        tokeniser = tokenisers.AaTokeniser(len_tuplet=1)
+
+        aa_seq = 'CASYF'
+        expected = [3,2,17,21,6]
+
+        result_x, result_y = tokeniser.generate_mlm_pair(
+            aa_seq=aa_seq,
+            p_mask=0.4,
+            p_mask_random=1,
+            p_mask_keep=0
+        )
+
+        masked_indices = [i for i, t in enumerate(result_y) if t != 0]
+
+        assert all(
+            [
+                result_x[i] in range(2,22) and \
+                result_x[i] != expected[i] for i in masked_indices
+            ]
+        )
+
+
+    def test_generate_mlm_pair_keep_token(self):
+        tokeniser = tokenisers.AaTokeniser(len_tuplet=1)
+
+        aa_seq = 'CASYF'
+        expected = [3,2,17,21,6]
+
+        result_x, result_y = tokeniser.generate_mlm_pair(
+            aa_seq=aa_seq,
+            p_mask=0.4,
+            p_mask_random=0,
+            p_mask_keep=1
+        )
+
+        masked_indices = [i for i, t in enumerate(result_y) if t != 0]
+
+        assert all([result_x[i] == expected[i] for i in masked_indices])
+
+
+    @pytest.mark.parametrize(
+        'len_tuplet', (-1, 'foo', None)
+    )
+    def test_error_bad_len_tuplet(self, len_tuplet):
+        with pytest.raises(Exception):
+            tokenisers.AaTokeniser(len_tuplet=len_tuplet)
+
+
+    @pytest.mark.parametrize(
+        'p_mask', (-0.1, 1)
+    )
+    def test_error_generate_mlm_pair_bad_p_mask(self, p_mask):
+        tokeniser = tokenisers.AaTokeniser(len_tuplet=1)
+
+        with pytest.raises(RuntimeError):
+            tokeniser.generate_mlm_pair(
+                aa_seq='CASYF',
+                p_mask=p_mask,
+                p_mask_random=0,
+                p_mask_keep=0
+            )
+
+
+    @pytest.mark.parametrize(
+        'p_mask_random', (-0.1, 1.1)
+    )
+    def test_error_generate_mlm_pair_bad_p_mask_random(self, p_mask_random):
+        tokeniser = tokenisers.AaTokeniser(len_tuplet=1)
+
+        with pytest.raises(RuntimeError):
+            tokeniser.generate_mlm_pair(
+                aa_seq='CASYF',
+                p_mask=0.5,
+                p_mask_random=p_mask_random,
+                p_mask_keep=0
+            )
+
+
+    @pytest.mark.parametrize(
+        'p_mask_keep', (-0.1, 1.1)
+    )
+    def test_error_generate_mlm_pair_bad_p_mask_keep(self, p_mask_keep):
+        tokeniser = tokenisers.AaTokeniser(len_tuplet=1)
+
+        with pytest.raises(RuntimeError):
+            tokeniser.generate_mlm_pair(
+                aa_seq='CASYF',
+                p_mask=0.5,
+                p_mask_random=0,
+                p_mask_keep=p_mask_keep
+            )
+
+
+    def test_error_generate_mlm_pair_bad_p_mask_random_keep(self):
+        tokeniser = tokenisers.AaTokeniser(len_tuplet=1)
+
+        with pytest.raises(RuntimeError):
+            tokeniser.generate_mlm_pair(
+                aa_seq='CASYF',
+                p_mask=0.5,
+                p_mask_random=0.5,
+                p_mask_keep=0.6
+            )
