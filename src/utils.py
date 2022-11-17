cc71d2caefd2d72c83ab1761916cdc3f5286e0cf
Yuta <52748151+yutanagano@users.noreply.github.com>
2022-11-17 13:54:21 +0000

    Implement saving function

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,12 @@ Utility classes and functions.
 '''
 
 
+import json
+import pandas as pd
+from pathlib import Path
+import torch
 from torch import Tensor
+from torch.nn import Module
 
 
 def masked_average_pool(
@@ -36,3 +41,45 @@ def masked_average_pool(
     token_embeddings_averaged = token_embeddings_summed / padding_mask.sum(1)
 
     return token_embeddings_averaged
+
+
+def save(
+    wd: Path,
+    save_name: str,
+    model: Module,
+    log: dict,
+    config: dict
+) -> None:
+    model_saves_dir = wd/'model_saves'
+    try:
+        model_saves_dir.mkdir()
+    except(FileExistsError):
+        pass
+
+    try:
+        (model_saves_dir/save_name).mkdir()
+    except(FileExistsError):
+        suffix_int = 1
+        new_save_name = f'{save_name}_{suffix_int}'
+        done = False
+        while not done:
+            print(save_name)
+            try:
+                (model_saves_dir/new_save_name).mkdir()
+                save_name = new_save_name
+                done = True
+            except(FileExistsError):
+                suffix_int += 1
+                new_save_name = f'{save_name}_{suffix_int}'
+    save_dir = model_saves_dir/save_name
+
+    # Save model
+    torch.save(model.state_dict(), save_dir/'state_dict.pt')
+
+    # Save log
+    pd.DataFrame.from_dict(log, orient='index')\
+        .to_csv(save_dir/'log.csv', index=False)
+
+    # Save config
+    with open(save_dir/'config.json', 'w') as f:
+        json.dump(config, f)
