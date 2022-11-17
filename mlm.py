04711bcf9976555152dc6ef50d6d25bdc5ee0404
Yuta <52748151+yutanagano@users.noreply.github.com>
2022-11-17 14:03:04 +0000

    Reimplement MLM training loop

--- mlm.py
+++ mlm.py
@@ -4,15 +4,20 @@ An executable script to conduct masked-language modelling on TCR models.
 
 
 import argparse
+from datetime import datetime
 import json
 from pathlib import Path
 from src import modules
+from src.modules.embedder import MLMEmbedder
 from src.datahandling import tokenisers
 from src.datahandling.dataloaders import TCRDataLoader
 from src.datahandling.datasets import TCRDataset
-from src.metrics import AdjustedCELoss
+from src.metrics import AdjustedCELoss, mlm_acc, mlm_topk_acc
 from src.optim import AdamWithScheduling
+from src.utils import save
 import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 from typing import Union
 
 
@@ -40,7 +45,68 @@ def parse_command_line_arguments() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def train(device: Union[str, int], wd: Path, config: dict):
+def train(
+    model: MLMEmbedder,
+    dl: DataLoader,
+    loss_fn,
+    optimiser,
+    device
+) -> dict:
+    model.train()
+
+    total_loss = 0
+    divisor = 0
+
+    for x, y in tqdm(dl):
+        num_samples = len(x)
+
+        x = x.to(device)
+        y = y.to(device)
+        logits = model.mlm(x)
+
+        optimiser.zero_grad()
+        loss = loss_fn(logits.flatten(0,1), y.view(-1))
+        loss.backward()
+        optimiser.step()
+
+        total_loss += loss.item() * num_samples
+        divisor += num_samples
+
+    return {'loss': total_loss / divisor}
+
+
+@torch.no_grad
+def validate(model: MLMEmbedder, dl: DataLoader, loss_fn, device) -> dict:
+    model.eval()
+
+    total_loss = 0
+    total_acc = 0
+    total_top5_acc = 0
+    divisor = 0
+
+    for x, y in tqdm(dl):
+        num_samples = len(x)
+
+        x = x.to(device)
+        y = y.to(device)
+
+        logits = model.mlm(x)
+
+        loss = loss_fn(logits.flatten(0,1), y.view(-1))
+
+        total_loss += loss.item() * num_samples
+        total_acc += mlm_acc(logits, y) * num_samples
+        total_top5_acc += mlm_topk_acc(logits, y, 5) * num_samples
+        divisor += num_samples
+
+    return {
+        'valid_loss': total_loss / divisor,
+        'valid_acc': total_acc / divisor,
+        'valid_top5_acc': total_top5_acc / divisor
+    }
+
+
+def mlm(device: Union[str, int], wd: Path, config: dict):
     # Set device
     device = torch.device(device)
 
@@ -66,21 +132,30 @@ def train(device: Union[str, int], wd: Path, config: dict):
     # Instantiate model
     model = MODELS[config['model']](**config['model_config']).to(device)
 
-    # Instantiate loss and optimiser
-    loss = AdjustedCELoss(label_smoothing=0.1)
+    # Instantiate loss function and optimiser
+    loss_fn = AdjustedCELoss(label_smoothing=0.1)
     optimiser = AdamWithScheduling(
         params=model.parameters(),
         d_model=config['model_config']['d_model'],
         **config['optimiser_config']
     )
 
+    metric_log = dict()
+
+    # Go through epochs of training
     for epoch in range(1, config['n_epochs']+1):
-        # TODO: do epoch through training data
-        # TODO: compute validation metrics
-        # TODO: log metrics
-        pass
+        train_metrics = train(model, train_dl, loss_fn, optimiser, device)
+        valid_metrics = validate(model, valid_dl, loss_fn, device)
+        metric_log[epoch] = {**train_metrics, **valid_metrics}
     
-    # TODO: save results
+    # Save results
+    save(
+        wd=wd,
+        save_name=datetime.now().strftime(r"%Y%m%d_%H%M%S"),
+        model=model,
+        log=metric_log,
+        config=config
+    )
 
 
 def main(wd: Path, config: dict):
@@ -88,11 +163,11 @@ def main(wd: Path, config: dict):
         return
 
     if config['n_gpus'] == 1:
-        train(device=0, wd=wd, config=config)
+        mlm(device=0, wd=wd, config=config)
         return
 
     if config['n_gpus'] == 0:
-        train(device='cpu', wd=wd, config=config)
+        mlm(device='cpu', wd=wd, config=config)
         return
 
 
@@ -103,6 +178,8 @@ if __name__ == '__main__':
         wd = Path.cwd()
     else:
         wd = Path(args.working_directory).resolve()
+
+    assert wd.is_dir()
 
     with open(args.config_path, 'r') as f:
         config = json.load(f)
