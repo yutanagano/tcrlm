40b93c63c9ba5659919ff744aecb9f1614a734a1
Yuta <52748151+yutanagano@users.noreply.github.com>
2022-11-19 20:59:58 +0000

    Test sinusoidal positional embeddings

---
+++ tests/0_unit_tests/test_sinposembedding.py
@@ -0,0 +1,16 @@
+from src.modules.bert.embedding import SinPositionEmbedding
+import torch
+
+
+class TestSinPositionEmbedding:
+    def test_positional_embedding(self):
+        embedding = SinPositionEmbedding(num_embeddings=2, embedding_dim=4)
+        result = embedding(torch.tensor([[1,2],[1,0]], dtype=torch.long))
+        expected = torch.tensor(
+            [
+                [[0.0000,1.0000,0.0000,1.0000],[0.8415,0.5403,0.1816,0.9834]],
+                [[0.0000,1.0000,0.0000,1.0000],[0.0000,0.0000,0.0000,0.0000]]
+            ]
+        )
+
+        torch.testing.assert_close(result, expected, rtol=0.001, atol=0)
