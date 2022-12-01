f4a51afa044f86b93b8bb9ab992eee373db59921
Yuta <52748151+yutanagano@users.noreply.github.com>
2022-12-01 14:11:00 +0000

    Update alignment and uniformity metrics

--- tests/0_unit_tests/test_metrics.py
+++ tests/0_unit_tests/test_metrics.py
@@ -10,8 +10,8 @@ class TestAlignment:
         labels = torch.tensor([0,0,1,1])
 
         torch.testing.assert_close(
-            metrics.alignment(x, labels, alpha=2),
-            torch.tensor(2, dtype=torch.float32)
+            metrics.alignment(x, labels),
+            torch.tensor(2, dtype=torch.float32).sqrt()
         )
 
 
@@ -21,8 +21,8 @@ class TestAlignmentPaired:
         z_prime = torch.tensor([[1,0],[-1,0]], dtype=torch.float32)
 
         torch.testing.assert_close(
-            metrics.alignment_paired(z, z_prime, alpha=2),
-            torch.tensor(2, dtype=torch.float32)
+            metrics.alignment_paired(z, z_prime),
+            torch.tensor(2, dtype=torch.float32).sqrt()
         )
 
 
@@ -31,12 +31,10 @@ class TestUniformity:
         x = torch.tensor([[0,1],[1,0],[0,-1],[-1,0]], dtype=torch.float32)
 
         torch.testing.assert_close(
-            metrics.uniformity(x,t=2),
-            torch.log(torch.tensor(1/3)) + \
-                torch.log(
-                    torch.tensor(2.0) + torch.exp(torch.tensor(-4.0))
-                ) - \
-                torch.tensor(4.0)
+            metrics.uniformity(x),
+            torch.log(
+                2*torch.tensor(2).sqrt().neg().exp() + torch.tensor(-2).exp()
+            ) - torch.tensor(3).log()
         )
 
 
