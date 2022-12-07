4fdfdcff557999deb16e4b442228fa5d6880af8d
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2022-12-07 17:05:18 +0000

    Implement new unsupervised contrastive dataset that can randomly censor chains

--- src/datahandling/tokenisers.py
+++ src/datahandling/tokenisers.py
@@ -16,7 +16,7 @@ describe chain, position, etc.), there are two more reserved indices:
 
 
 from abc import ABC, abstractmethod
-from pandas import notna, Series
+from pandas import isna, notna, Series
 import torch
 from torch import Tensor
 
@@ -41,7 +41,7 @@ class Tokeniser(ABC):
 
 
     @abstractmethod
-    def tokenise(self, tcr: Series) -> Tensor:
+    def tokenise(self, tcr: Series, chain: str = 'both') -> Tensor:
         '''
         Given a pandas Series containing information on a particular TCR,
         generate a tokenisation of it.
@@ -66,9 +66,9 @@ class CDR3Tokeniser(Tokeniser):
         return 20
 
 
-    def tokenise(self, tcr: Series) -> Tensor:
+    def tokenise(self, tcr: Series, chain: str = 'both') -> Tensor:
         '''
-        Tokenise a TCR in terms of its alpha and beta chain CDR3 amino acid
+        Tokenise a TCR in terms of its alpha and/or beta chain CDR3 amino acid
         sequences.
 
         Amino acids get mapped as in the following: 'A' -> 3, 'C' -> 4, ...
@@ -81,19 +81,30 @@ class CDR3Tokeniser(Tokeniser):
             above), the second element is an integer indicating whether the
             residue came from the alpha (1) or beta (2) CDR3, and the third
             element is an integer indicating the residue position within its
-            chain (1-indexed).
+            chain (1-indexed). Depending on the input for `chain`, either both,
+            only alpha, or only the beta chain will be tokenised, unless the
+            specified chain is not available, in which case it will default
+            back to tokenising the available chain.
         '''
+        if not chain in ('both', 'alpha', 'beta'):
+            raise ValueError(f'Unrecognised value for chain: {chain}')
 
         cdr3a = tcr.loc['CDR3A']
         cdr3b = tcr.loc['CDR3B']
 
         tokenised = [[2,0,0]]
 
-        if notna(cdr3a):
+        if isna(cdr3a) and chain == 'alpha':
+            chain = 'beta'
+
+        if isna(cdr3b) and chain == 'beta':
+            chain = 'alpha'
+
+        if notna(cdr3a) and chain in ('both', 'alpha'):
             for i, aa in enumerate(cdr3a):
                 tokenised.append([self._aa_to_index[aa], 1, i+1])
 
-        if notna(cdr3b):
+        if notna(cdr3b) and chain in ('both', 'beta'):
             for i, aa in enumerate(cdr3b):
                 tokenised.append([self._aa_to_index[aa], 2, i+1])
 
