277b88ce8d8f6a9abe3fc82a1158c7e881634ec6
Yuta <52748151+yutanagano@users.noreply.github.com>
2022-12-20 19:40:15 +0900

    Implement EpitopeContrastive model class

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,3 +1,8 @@
 from .atchleyembedder import AtchleyEmbedder
 from .randomembedder import RandomEmbedder
-from .bert.cdr3bert import CDR3BERT_c, CDR3BERT_cp, AutoContrastive_CDR3BERT_cp
+from .bert.cdr3bert import (
+    CDR3BERT_c,
+    CDR3BERT_cp,
+    AutoContrastive_CDR3BERT_cp,
+    EpitopeContrastive_CDR3BERT_cp
+)
