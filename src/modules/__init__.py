30cfaccacf384650ddf196f3bb332ec93cda2ef2
Yuta <52748151+yutanagano@users.noreply.github.com>
2023-01-09 19:43:53 +0900

    Write new class for CDR3BERT model with only amino acid information

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,6 +1,7 @@
 from .atchleyembedder import AtchleyEmbedder
 from .randomembedder import RandomEmbedder
 from .bert.cdr3bert import (
+    CDR3BERT_a,
     CDR3BERT_ac,
     CDR3BERT_acp,
     AutoContrastive_CDR3BERT_acp,
--- tests/0_unit_tests/test_modules_cdr3bertac.py
+++ tests/0_unit_tests/test_modules_cdr3bertac.py
@@ -1,11 +1,11 @@
 import pytest
-from src.modules import CDR3BERT_acp
+from src.modules import CDR3BERT_ac
 import torch
 
 
 @pytest.fixture
-def cdr3bert_cp():
-    model = CDR3BERT_acp(
+def cdr3bert_ac():
+    model = CDR3BERT_ac(
         num_encoder_layers=6,
         d_model=64,
         nhead=8,
@@ -15,25 +15,25 @@ def cdr3bert_cp():
     return model
 
 
-class TestCDR3BERT_cp:
-    def test_init_attributes(self, cdr3bert_cp):
-        assert cdr3bert_cp.embed_layer == 5
-        assert cdr3bert_cp._num_layers == 6
-        assert cdr3bert_cp._d_model == 64
-        assert cdr3bert_cp._nhead == 8
-        assert cdr3bert_cp._dim_feedforward == 256
+class TestCDR3BERT_ac:
+    def test_init_attributes(self, cdr3bert_ac):
+        assert cdr3bert_ac.embed_layer == 5
+        assert cdr3bert_ac._num_layers == 6
+        assert cdr3bert_ac._d_model == 64
+        assert cdr3bert_ac._nhead == 8
+        assert cdr3bert_ac._dim_feedforward == 256
 
 
-    def test_forward(self, cdr3bert_cp):
+    def test_forward(self, cdr3bert_ac):
         batch = torch.zeros((3,10,3), dtype=torch.long)
-        out, padding_mask = cdr3bert_cp(x=batch)
+        out, padding_mask = cdr3bert_ac(x=batch)
 
         assert out.size() == (3,10,64)
         assert padding_mask.size() == (3,10)
         assert (padding_mask == 1).all()
 
 
-    def test_embed(self, cdr3bert_cp):
+    def test_embed(self, cdr3bert_ac):
         batch = torch.tensor(
             [
                 [[3,1,1],[4,1,2],[5,1,3],[3,2,1],[4,2,2],[5,2,3]],
@@ -42,18 +42,18 @@ class TestCDR3BERT_cp:
             ],
             dtype=torch.long
         )
-        out = cdr3bert_cp.embed(x=batch)
+        out = cdr3bert_ac.embed(x=batch)
 
         assert out.size() == (3,64)
         torch.testing.assert_close(out.norm(dim=1), torch.ones(3))
 
 
-    def test_mlm(self, cdr3bert_cp):
+    def test_mlm(self, cdr3bert_ac):
         batch = torch.zeros((3,10,3), dtype=torch.long)
-        out = cdr3bert_cp.mlm(x=batch)
+        out = cdr3bert_ac.mlm(x=batch)
 
         assert out.size() == (3,10,20)
 
 
-    def test_name(self, cdr3bert_cp):
-        assert cdr3bert_cp.name == 'CDR3BERT_acp_6_64_8_256-embed_5'
+    def test_name(self, cdr3bert_ac):
+        assert cdr3bert_ac.name == 'CDR3BERT_ac_6_64_8_256-embed_5'
