decf9a3c173f57b0fc577dc1642f6091f224d045
Yuta <52748151+yutanagano@users.noreply.github.com>
2023-01-25 09:15:31 +0000

    Implement beta chain autocontrastive model

--- src/modules/bert/cdr3bert.py
+++ src/modules/bert/cdr3bert.py
@@ -249,6 +249,33 @@ class _ConCDR3BERTBase_acp(BERTClsEmbedBase, CDR3BERT_acp):
         self._name_base = self._name_base + '_' + contrastive_loss_type
 
 
+class _ConBetaCDR3BERTBase_ap(BERTClsEmbedBase, BetaCDR3BERT_ap):
+    '''
+    BetaCDR3BERT_ap model which uses the <cls> token to embed.
+    '''
+
+
+    def __init__(
+        self,
+        contrastive_loss_type: str,
+        num_encoder_layers: int,
+        d_model: int,
+        nhead: int,
+        dim_feedforward: int,
+        dropout: float = 0.1
+    ) -> None:
+        super().__init__(
+            num_encoder_layers,
+            d_model,
+            nhead,
+            dim_feedforward,
+            dropout
+        )
+
+        self._name_base = self._name_base.lstrip('Beta')
+        self._name_base = self._name_base + '_' + contrastive_loss_type
+
+
 class AutoContCDR3BERT_acp(_ConCDR3BERTBase_acp):
     '''
     CDR3BERT_acp model embedding using the <cls> token and with base name
@@ -257,6 +284,16 @@ class AutoContCDR3BERT_acp(_ConCDR3BERTBase_acp):
 
     
     _name_base = 'AutoContrastive_CDR3BERT'
+
+
+class AutoContBetaCDR3BERT_ap(_ConBetaCDR3BERTBase_ap):
+    '''
+    BetaCDR3BERT_ap model embedding using the <cls> token and with base name
+    'AutoContrastive...'
+    '''
+
+    
+    _name_base = 'AutoContrastive_BetaCDR3BERT'
 
 
 class EpContCDR3BERT_acp(_ConCDR3BERTBase_acp):
