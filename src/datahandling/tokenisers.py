ce97a21d583c869be61e12858d1d63590f3b5f4d
Yuta <52748151+yutanagano@users.noreply.github.com>
2023-02-15 13:08:11 +0900

    Implement censoring noising for BCDRTokeniser

--- src/datahandling/tokenisers.py
+++ src/datahandling/tokenisers.py
@@ -112,6 +112,11 @@ class BCDR3Tokeniser(_AATokeniser):
     '''
 
 
+    def __init__(self, p_drop_aa: float = 0.2) -> None:
+        super().__init__()
+        self._p_drop_aa = p_drop_aa
+
+
     def tokenise(self, tcr: Series, noising: bool = False) -> Tensor:
         '''
         Tokenise a TCR in terms of its beta chain CDR3 amino acid sequences.
@@ -132,7 +137,8 @@ class BCDR3Tokeniser(_AATokeniser):
 
         cdr3b_size = len(cdr3b)
         for i, aa in enumerate(cdr3b):
-            if noising and random.random() < 0.2:
+            # If noising, randomly drop some AAs
+            if noising and random.random() < self._p_drop_aa:
                 continue
 
             tokenised.append([self._aa_to_index[aa], i+1, cdr3b_size])
@@ -201,7 +207,17 @@ class BCDRTokeniser(_AATokeniser):
     '''
 
 
-    def tokenise(self, tcr: Series) -> Tensor:
+    def __init__(
+        self,
+        p_drop_cdr: float = 0.2,
+        p_drop_aa: float = 0.2
+    ) -> None:
+        super().__init__()
+        self._p_drop_cdr = p_drop_cdr
+        self._p_drop_aa = p_drop_aa
+
+
+    def tokenise(self, tcr: Series, noising: bool = False) -> Tensor:
         '''
         Tokenise a TCR in terms of the amino acid sequences of its 3 CDRs.
 
@@ -216,31 +232,55 @@ class BCDRTokeniser(_AATokeniser):
             Dim 3 - compartment ID
         '''
 
-
+        # Get the CDRs
         trbv = tcr.loc['TRBV']
         cdr3b = tcr.loc['CDR3B']
 
         cdr1b = None if isna(trbv) else V_CDRS[trbv]['CDR1-IMGT']
         cdr2b = None if isna(trbv) else V_CDRS[trbv]['CDR2-IMGT']
 
-        tokenised = [[2,0,0,0]]
-
-        if notna(cdr1b):
-            cdr1b_size = len(cdr1b)
-            for i, aa in enumerate(cdr1b):
-                tokenised.append([self._aa_to_index[aa], i+1, cdr1b_size, 1])
-
-        if notna(cdr2b):
-            cdr2b_size = len(cdr2b)
-            for i, aa in enumerate(cdr2b):
-                tokenised.append([self._aa_to_index[aa], i+1, cdr2b_size, 2])
-
-        if notna(cdr3b):
-            cdr3b_size = len(cdr3b)
-            for i, aa in enumerate(cdr3b):
-                tokenised.append([self._aa_to_index[aa], i+1, cdr3b_size, 3])
-
-        if len(tokenised) == 1:
+        if not (cdr1b or cdr2b or cdr3b):
             raise ValueError(f'No TCRB data found in row {tcr.name}.')
 
+        # If noising, potentially drop some CDRs
+        include_cdr1 = include_cdr2 = include_cdr3 = True
+
+        if noising:
+            include_cdr1 = random.random() < self._p_drop_cdr
+            include_cdr2 = random.random() < self._p_drop_cdr
+            include_cdr3 = random.random() < self._p_drop_cdr
+
+            # Make sure filter doesn't cause the whole TCR to be censored
+            if not (
+                (cdr1b and include_cdr1) or 
+                (cdr2b and include_cdr2) or 
+                (cdr3b and include_cdr3)
+            ):
+                include_cdr1 = include_cdr2 = include_cdr3 = True
+
+        tokenised = [[2,0,0,0]]
+
+        if (cdr1b and include_cdr1):
+            tokenised.extend(self._tokenise_cdr(cdr1b, 1, noising))
+
+        if (cdr2b and include_cdr2):
+            tokenised.extend(self._tokenise_cdr(cdr2b, 2, noising))
+
+        if (cdr3b and include_cdr3):
+            tokenised.extend(self._tokenise_cdr(cdr3b, 3, noising))
+
         return torch.tensor(tokenised, dtype=torch.long)
+    
+
+    def _tokenise_cdr(self, cdr: str, cmpt_idx: int, noising: bool) -> list:
+        cdr_size = len(cdr)
+
+        tokenised = []
+        for i, aa in enumerate(cdr):
+            # If noising, randomly drop some AAs
+            if noising and random.random() < self._p_drop_aa:
+                continue
+
+            tokenised.append([self._aa_to_index[aa], i+1, cdr_size, cmpt_idx])
+
+        return tokenised
