a49038fe0116c6587ca82bd84e70a012b9e6131f
Yuta <52748151+yutanagano@users.noreply.github.com>
2023-03-15 13:53:33 +0000

    Implement CDRBERT

--- tests/0_unit_tests/test_datahandling_tokenisers.py
+++ tests/0_unit_tests/test_datahandling_tokenisers.py
@@ -16,7 +16,7 @@ def mock_data_beta_df():
     return df
 
 
-class TestABCDR3Tokeniser:
+class TestCDR3Tokeniser:
     def test_tokenise(self, mock_data_df):
         tokeniser = tokenisers.CDR3Tokeniser()
 
@@ -118,4 +118,36 @@ class TestBCDRTokeniser:
         ]
 
         for (_, item), target in zip(mock_data_beta_df.iterrows(), expected):
+            assert torch.equal(tokeniser.tokenise(item), target)
+
+
+class TestCDRTokeniser:
+    def test_tokenise(self, mock_data_df):
+        tokeniser = tokenisers.CDRTokeniser(p_drop_aa=0, p_drop_cdr=0, p_drop_chain=0)
+
+        expected = [
+            torch.tensor([
+                [2,0,0,0],
+                [19,1,6,1],[18,2,6,1],[8,3,6,1],[7,4,6,1],[22,5,6,1],[8,6,6,1], # TSGFYG
+                [14,1,6,2],[3,2,6,2],[12,3,6,2],[5,4,6,2],[8,5,6,2],[12,6,6,2], # NALDGL
+                [4,1,6,3],[3,2,6,3],[18,3,6,3],[16,4,6,3],[22,5,6,3],[7,6,6,3], # CASQYF
+                [18,1,5,4],[14,2,5,4],[9,3,5,4],[12,4,5,4],[22,5,5,4], #SNHLY
+                [7,1,6,5],[22,2,6,5],[14,3,6,5],[14,4,6,5],[6,5,6,5],[10,6,6,5], #FYNNEI
+                [4,1,5,6],[3,2,5,6],[19,3,5,6],[22,4,5,6],[21,5,5,6] #CATYW
+            ]),
+            torch.tensor([
+                [2,0,0,0],
+                [19,1,6,1],[18,2,6,1],[8,3,6,1],[7,4,6,1],[22,5,6,1],[8,6,6,1], # TSGFYG
+                [14,1,6,2],[3,2,6,2],[12,3,6,2],[5,4,6,2],[8,5,6,2],[12,6,6,2], # NALDGL
+                [4,1,6,3],[3,2,6,3],[18,3,6,3],[16,4,6,3],[22,5,6,3],[7,6,6,3], # CASQYF
+            ]),
+            torch.tensor([
+                [2,0,0,0],
+                [18,1,5,4],[14,2,5,4],[9,3,5,4],[12,4,5,4],[22,5,5,4], #SNHLY
+                [7,1,6,5],[22,2,6,5],[14,3,6,5],[14,4,6,5],[6,5,6,5],[10,6,6,5], #FYNNEI
+                [4,1,5,6],[3,2,5,6],[19,3,5,6],[22,4,5,6],[21,5,5,6] #CATYW
+            ])
+        ]
+
+        for (_, item), target in zip(mock_data_df.iterrows(), expected):
             assert torch.equal(tokeniser.tokenise(item), target)
