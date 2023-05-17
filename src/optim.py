30f84b1fc1a8d435070d008325ae3ce29e6b47b0
Yuta <52748151+yutanagano@users.noreply.github.com>
2023-05-17 08:52:02 +0100

    Code cleaning

--- src/optim.py
+++ src/optim.py
@@ -14,15 +14,19 @@ class AdamWithScheduling:
         params,
         d_model: int,
         n_warmup_steps: int,
+        lr: float = 0.001,
         lr_multiplier: float = 1,
+        decay: bool = True,
         **kwargs
     ):
-        self.optimiser = torch.optim.Adam(params=params, **kwargs)
+        self.optimiser = torch.optim.Adam(params=params, lr=lr, **kwargs)
         self._d_model = d_model
         self._n_warmup_steps = n_warmup_steps
         self._lr_multiplier = lr_multiplier
+        self._decay = decay
 
         self._step_num = 1
+        self._lr_explicit = lr
 
     @property
     def lr(self) -> float:
@@ -42,10 +46,16 @@ class AdamWithScheduling:
 
     def calculate_lr(self, step_num: int) -> float:
         # Learning rate decays inversely with the square root of step number
-        return (
-            self._lr_multiplier
-            * self._d_model**-0.5
-            * min(step_num ** (-0.5), step_num * self._n_warmup_steps ** (-1.5))
+        if self._decay:
+            return (
+                self._lr_multiplier
+                * self._d_model**-0.5
+                * min(step_num ** (-0.5), step_num * self._n_warmup_steps ** (-1.5))
+            )
+
+        # Learning rate reaches target and stays
+        return min(
+            self._lr_explicit, step_num / self._n_warmup_steps * self._lr_explicit
         )
 
     def _update_lr(self) -> None:
