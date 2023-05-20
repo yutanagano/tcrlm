6f12be19f49503047e87f35cc187cd834a2322c2
Yuta <52748151+yutanagano@users.noreply.github.com>
2023-05-20 19:25:46 +0100

    Further cleaned becnhmarking code and produced new levenshtein benchmarks

--- benchmark_beta.py
+++ benchmark_beta.py
@@ -3,14 +3,8 @@ Benchmark models using beta-chain only data.
 """
 
 
-from argparse import ArgumentParser
-from pathlib import Path
 from src.pipelines import BetaBenchmarkingPipeline
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Benchmarking pipeline.")
-    parser.add_argument("model_save_dir", help="Path to model save directory.")
-    args = parser.parse_args()
-
-    BetaBenchmarkingPipeline.main(Path(args.model_save_dir))
+    BetaBenchmarkingPipeline.run_from_clargs()
--- src/pipelines/__init__.py
+++ src/pipelines/__init__.py
@@ -1,9 +1,9 @@
-from .benchmarking_pipelines.benchmarking_pipeline import BenchmarkingPipeline
-from .benchmarking_pipelines.beta_benchmarking_pipeline import BetaBenchmarkingPipeline
+from .benchmarking_pipelines.benchmarking_pipeline import BenchmarkingPipeline, BetaBenchmarkingPipeline
 from .benchmarking_pipelines.tcrdist_benchmarking_pipeline import (
     TcrdistBenchmarkingPipeline,
     BTcrdistBenchmarkingPipeline,
 )
+from .benchmarking_pipelines.levenshtein_benchmarking_pipeline import CDR3BLevenshteinBenchmarkingPipeline
 
 from .training_pipelines.contrastive_pipelines import ACLPipeline, ECLPipeline
 from .training_pipelines.mlm_pipeline import MLMPipeline
