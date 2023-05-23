e11b1b4988e5b89c9e9917623bbd8790d09800fa
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-05-23 10:44:19 +0100

    Implement cdrlevenshtein benchmarking

--- benchmark_beta_levenshtein.py
+++ benchmark_beta_levenshtein.py
@@ -3,8 +3,9 @@ Benchmark simple Levenshtein distances.
 """
 
 
-from src.pipelines import CDR3BLevenshteinBenchmarkingPipeline
+from src.pipelines import CDR3BLevenshteinBenchmarkingPipeline, CDRBLevenshteinBenchmarkingPipeline
 
 
 if __name__ == "__main__":
     CDR3BLevenshteinBenchmarkingPipeline.run_from_clargs()
+    CDRBLevenshteinBenchmarkingPipeline.run_from_clargs()
