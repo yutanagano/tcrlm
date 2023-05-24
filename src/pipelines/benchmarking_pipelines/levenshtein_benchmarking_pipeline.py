804cd9bc77ea9e88cd16c35beee151078bd12520
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-05-24 17:07:22 +0100

    Further work implementing pgen density investigation

--- src/pipelines/benchmarking_pipelines/levenshtein_benchmarking_pipeline.py
+++ src/pipelines/benchmarking_pipelines/levenshtein_benchmarking_pipeline.py
@@ -3,6 +3,7 @@ Simple levenshtein editions of the benchmarking pipeline.
 """
 
 
+from numpy import ndarray
 from .pure_metric_benchmarking_pipeline import PureMetricBenchmarkingPipeline
 from pandas import DataFrame
 from rapidfuzz.process import cdist
@@ -13,33 +14,55 @@ import tidytcells as tt
 class CDR3BLevenshteinBenchmarkingPipeline(PureMetricBenchmarkingPipeline):
     MODEL_NAME = "cdr3b_levenshtein"
 
-    def get_cdist_matrix(cls, ds_name: str, ds_df: DataFrame) -> tuple:
-        cdist_matrix = cdist(
-            ds_df["CDR3B"], ds_df["CDR3B"], scorer=Levenshtein.distance
-        ).astype(float)
-
-        return cdist_matrix
+    def get_pdist_matrix(cls, ds_name: str, ds_df: DataFrame) -> tuple:
+        return cls.get_cdist(ds_df, ds_df)
     
+    def get_cdist(cls, ds_a_df: DataFrame, ds_b_df: DataFrame) -> ndarray:
+        cdist_matrix = cdist(
+            ds_a_df["CDR3B"], ds_b_df["CDR3B"], scorer=Levenshtein.distance
+        ).astype(float)
+    
+        return cdist_matrix
 
 class CDRBLevenshteinBenchmarkingPipeline(PureMetricBenchmarkingPipeline):
     MODEL_NAME = "cdrb_levenshtein"
 
-    def get_cdist_matrix(cls, ds_name: str, ds_df: DataFrame) -> tuple:
-        ds_df["TRBV"] = ds_df["TRBV"].map(lambda x: x if "*" in x else x + "*01")
+    def load_data(cls) -> None:
+        super().load_data()
+        cls.background_data = cls.background_data.copy()[
+            cls.background_data["TRBV"].notna()
+        ].reset_index(drop=True)
 
-        ds_df["CDR1B"] = ds_df["TRBV"].map(lambda x: tt.tcr.get_aa_sequence(x)["CDR1-IMGT"])
-        ds_df["CDR2B"] = ds_df["TRBV"].map(lambda x: tt.tcr.get_aa_sequence(x)["CDR2-IMGT"])
+    def get_pdist_matrix(cls, ds_name: str, ds_df: DataFrame) -> tuple:
+        return cls.get_cdist(ds_df, ds_df)
+    
+    def get_cdist(cls, ds_a_df: DataFrame, ds_b_df: DataFrame) -> ndarray:
+        ds_a_df = ds_a_df.copy()
+        ds_b_df = ds_b_df.copy()
+
+        def fix_bv(df):
+            df["TRBV"] = df["TRBV"].map(lambda x: x if "*" in x else x + "*01")
+            return df
+
+        def get_v_gene_cdrs(df):
+            df["CDR1B"] = df["TRBV"].map(lambda x: tt.tcr.get_aa_sequence(x)["CDR1-IMGT"])
+            df["CDR2B"] = df["TRBV"].map(lambda x: tt.tcr.get_aa_sequence(x)["CDR2-IMGT"])
+            return df
+
+        ds_a_df = fix_bv(ds_a_df)
+        ds_b_df = fix_bv(ds_b_df)
+
+        ds_a_df = get_v_gene_cdrs(ds_a_df)
+        ds_b_df =get_v_gene_cdrs(ds_b_df)
 
         cdist_cdr1 = cdist(
-            ds_df["CDR1B"], ds_df["CDR1B"], scorer=Levenshtein.distance
+            ds_a_df["CDR1B"], ds_b_df["CDR1B"], scorer=Levenshtein.distance
         ).astype(float)
         cdist_cdr2 = cdist(
-            ds_df["CDR2B"], ds_df["CDR2B"], scorer=Levenshtein.distance
+            ds_a_df["CDR2B"], ds_b_df["CDR2B"], scorer=Levenshtein.distance
         ).astype(float)
         cdist_cdr3 = cdist(
-            ds_df["CDR3B"], ds_df["CDR3B"], scorer=Levenshtein.distance
+            ds_a_df["CDR3B"], ds_b_df["CDR3B"], scorer=Levenshtein.distance
         ).astype(float)
-
-        cdist_matrix = cdist_cdr1 + cdist_cdr2 + cdist_cdr3
-
-        return cdist_matrix
+    
+        return cdist_cdr1 + cdist_cdr2 + cdist_cdr3
--- src/pipelines/benchmarking_pipelines/pure_metric_benchmarking_pipeline.py
+++ src/pipelines/benchmarking_pipelines/pure_metric_benchmarking_pipeline.py
@@ -4,7 +4,11 @@ Base class for benchmarking pure metrics.
 
 
 from .benchmarking_pipeline import BenchmarkingPipeline
+import numpy as np
+from numpy import ndarray
+from pandas import DataFrame
 from pathlib import Path
+from tqdm import tqdm
 
 
 class PureMetricBenchmarkingPipeline(BenchmarkingPipeline):
@@ -15,6 +19,7 @@ class PureMetricBenchmarkingPipeline(BenchmarkingPipeline):
 
     def main(cls) -> None:
         cls.setup(None)
+        cls.evaluate_pgen_vs_representation_space_density()
         for ds_name, ds_df in cls.labelled_data.items():
             cls.becnhmark_on_labelled_data(ds_name, ds_df)
         cls.save()
@@ -22,3 +27,22 @@ class PureMetricBenchmarkingPipeline(BenchmarkingPipeline):
 
     def instantiate_model(cls, model_save_dir: Path) -> None:
         cls.model = type("", (), {"name": cls.MODEL_NAME})
+
+    def get_avg_dist_to_100nn_over_background(cls) -> ndarray:
+        avg_dists = []
+
+        for i in tqdm(range(0, len(cls.background_data), 1000)):
+            dists_batch = cls.get_cdist(
+                cls.background_data.iloc[i:i+1000],
+                cls.background_data
+            ).squeeze()
+
+            for dists in dists_batch:
+                dists_to_closest_100 = np.partition(dists, kth=100)[:100]
+                avg_dist = dists_to_closest_100.mean()
+                avg_dists.append(avg_dist)
+
+        return np.array(avg_dists, dtype=np.float32)
+    
+    def get_cdist(cls, ds_a_df: DataFrame, ds_b_df: DataFrame) -> ndarray:
+        raise NotImplementedError()
