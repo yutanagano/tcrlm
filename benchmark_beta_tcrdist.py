b6e087f7da20de824f036afb7bd53b28f25c90c6
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-08-17 16:03:12 +0900

    Remove deprecated scripts

--- benchmark_beta_tcrdist.py
+++
@@ -1,9 +0,0 @@
-"""
-Benchmark models using beta-chain only data.
-"""
-
-
-from src.pipelines import BTcrdistBenchmarkingPipeline
-
-if __name__ == "__main__":
-    BTcrdistBenchmarkingPipeline.main()
--- ccl.py
+++
@@ -1,9 +0,0 @@
-"""
-An executable script to conduct epitope label-based contrastive learning on TCR
-models using labelled TCR data.
-"""
-
-from src.pipelines import CCLPipeline
-
-if __name__ == "__main__":
-    CCLPipeline().run_from_clargs()
