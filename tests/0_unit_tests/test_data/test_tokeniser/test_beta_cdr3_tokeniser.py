d04bde7438f378e9bdde52b7f1d68d3e79a6d0cf
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-08-31 11:54:51 +0900

    Test new BCDR3 tokenisation implementations

---
+++ tests/0_unit_tests/test_data/test_tokeniser/test_beta_cdr3_tokeniser.py
@@ -0,0 +1,34 @@
+import pytest
+import torch
+
+from src.data import make_tcr_from_components
+from src.data.tokeniser import BetaCdr3Tokeniser
+
+
+def test_tokenise(tokeniser, mock_tcr):
+    tokenised_tcr = tokeniser.tokenise(mock_tcr)
+    expected = torch.tensor(
+        [
+            [2, 0, 0],
+            [4, 1, 6],
+            [3, 2, 6],
+            [18, 3, 6],
+            [16, 4, 6],
+            [22, 5, 6],
+            [7, 6, 6],
+        ]
+    )
+
+    assert torch.equal(tokenised_tcr, expected)
+
+
+def test_tokenise_tcr_with_empty_beta_junction(tokeniser):
+    tcr_with_empty_beta = make_tcr_from_components("TRAV1-1*01", "CASQYF", "TRBV2*01", None)
+
+    with pytest.raises(RuntimeError):
+        tokeniser.tokenise(tcr_with_empty_beta)
+
+
+@pytest.fixture
+def tokeniser():
+    return BetaCdr3Tokeniser()
---
+++ tests/0_unit_tests/test_model/test_token_embedder/test_beta_cdr3_simple_embedder.py
@@ -0,0 +1,210 @@
+import pytest
+import torch
+from torch import Tensor
+
+from src.data.tokeniser import BetaCdr3Tokeniser
+from src.model.token_embedder import BetaCdr3SimpleEmbedder
+
+
+def test_forward(mock_batch_of_tokenised_tcrs):
+    BATCH_DIMENSIONALITY = 3
+    BATCH_SIZE = 2
+
+    embedder = BetaCdr3SimpleEmbedder()
+    batch_of_embedded_mock_tcrs = embedder.forward(mock_batch_of_tokenised_tcrs)
+    embedded_mock_tcr = batch_of_embedded_mock_tcrs[0]
+    expected_embedded_mock_tcr = torch.tensor(
+        [
+            [
+                0,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+            ],
+            [
+                0,
+                0,
+                0,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+            ],
+            [
+                0,
+                0,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0.2,
+            ],
+            [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0.4,
+            ],
+            [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0.6,
+            ],
+            [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                0.8,
+            ],
+            [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+            ],
+        ]
+    )
+
+    assert type(batch_of_embedded_mock_tcrs) == Tensor
+
+    assert batch_of_embedded_mock_tcrs.dim() == BATCH_DIMENSIONALITY
+    assert batch_of_embedded_mock_tcrs.size(0) == BATCH_SIZE
+
+    assert torch.equal(embedded_mock_tcr, expected_embedded_mock_tcr)
+
+
+@pytest.fixture
+def mock_batch_of_tokenised_tcrs(mock_tcr):
+    tokeniser = BetaCdr3Tokeniser()
+    tokenised_mock_tcr = tokeniser.tokenise(mock_tcr)
+    batch_of_tokenised_mock_tcrs = torch.stack([tokenised_mock_tcr, tokenised_mock_tcr])
+
+    return batch_of_tokenised_mock_tcrs
