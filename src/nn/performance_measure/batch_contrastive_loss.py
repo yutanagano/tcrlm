a9c4b08df6f6b9798c2a56ad2c9c6204b0ee391d
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-10-09 08:55:30 +0100

    Update batch contrastive loss and tcr schema object

--- src/nn/performance_measure/batch_contrastive_loss.py
+++ src/nn/performance_measure/batch_contrastive_loss.py
@@ -20,13 +20,18 @@ class BatchContrastiveLoss(Module):
         A(i) is the set of all elements other than i
         t is the temperature hyperparameter
 
-    and Loss_i is euivalent to (and here is computed in this way):
+    and Loss_i is euivalent to (and here is computed in this way*):
 
                                                         1
     Loss_i = log (    Σ      exp(z_i dot z_a / t) ) - ------    Σ      (z_i dot z_p / t)
                    a in A(i)                          |P(i)| p in P(i)
              ______________________________________   __________________________________
                         background term                         positives term
+    
+    *A caveat is that in order to prevent the exp terms from suffering from overflow, the dot products are all normalised.
+    This is done by subtracting the maximum dot product value of each row in the dot product matrix from all values in the same row.
+    However, due to the properties of softmax-like functions, this translation does not affect the computed loss value.
+    This procedure only serves to ensure that the computation behaves even with very low temperatures.
     """
 
     def __init__(self, temp: float = 0.05) -> None:
@@ -35,10 +40,16 @@ class BatchContrastiveLoss(Module):
 
     def forward(self, tcr_representations: FloatTensor, positives_mask: BoolTensor) -> FloatTensor:
         dot_products = torch.matmul(tcr_representations, tcr_representations.T) / self._temp
+        dot_products = self._ajust_dot_products_to_prevent_overflow(dot_products)
         positives_term = self._get_positives_term(dot_products, positives_mask)
         background_term = self._get_background_term(dot_products)
         loss_per_sample = background_term - positives_term
         return loss_per_sample.mean()
+    
+    def _ajust_dot_products_to_prevent_overflow(self, dot_products: FloatTensor) -> FloatTensor:
+        dot_products.fill_diagonal_(-1)
+        max_off_diagonal_value_from_each_row, _ = dot_products.max(dim=1, keepdim=True)
+        return dot_products - max_off_diagonal_value_from_each_row
 
     def _get_positives_term(self, dot_products: FloatTensor, positives_mask: BoolTensor) -> FloatTensor:
         ALONG_COMPARISONS_DIM = 1
