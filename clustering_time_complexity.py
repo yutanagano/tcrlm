a3b4216e1095ec0642249b08fd1ca5e819f324f5
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-11-20 17:18:36 +0000

    Better time complexity benchmarking code

--- clustering_time_complexity.py
+++ clustering_time_complexity.py
@@ -1,5 +1,6 @@
 import json
 import math
+import numpy as np
 import pandas as pd
 from pathlib import Path
 from pandas import DataFrame
@@ -7,6 +8,7 @@ from src.clustering import ClusteringMachine, BruteForceClusteringMachine, KdTre
 from src.model import tcr_metric, tcr_representation_model
 import statistics
 import timeit
+import torch
 
 
 def main() -> None:
@@ -15,22 +17,31 @@ def main() -> None:
 
 
 def get_time_complexity_stats() -> dict:
-    beta_cdr_levenshtein = tcr_metric.BetaCdrLevenshtein()
+    beta_cdr3_levenshtein = tcr_metric.BetaCdr3Levenshtein()
     beta_tcrdist = tcr_metric.BetaTcrdist()
-    blastr = tcr_representation_model.load_blastr_save(Path("/home/yutanagano/UCLOneDrive/MBPhD/projects/tcr_embedder/model_saves/Beta_CDR_BERT_Unsupervised_Large_2"))
 
-    levenshtein_bf = BruteForceClusteringMachine(beta_cdr_levenshtein)
+    sceptr_cpu = tcr_representation_model.load_blastr_save(Path("model_saves/SCEPTR"), device="cpu")
+    sceptr_cpu.name = "SCEPTR CPU"
+
+    sceptr_gpu = tcr_representation_model.load_blastr_save(Path("model_saves/SCEPTR"), device=0)
+    sceptr_gpu.name = "SCEPTR GPU"
+
+    levenshtein_bf = BruteForceClusteringMachine(beta_cdr3_levenshtein)
     tcrdist_bf = BruteForceClusteringMachine(beta_tcrdist)
-    blastr_bf = BruteForceClusteringMachine(blastr)
-    blastr_kdt = KdTreeClusteringMachine(blastr)
+    sceptr_cpu_bf = BruteForceClusteringMachine(sceptr_cpu)
+    sceptr_cpu_kdt = KdTreeClusteringMachine(sceptr_cpu)
+    sceptr_gpu_bf = BruteForceClusteringMachine(sceptr_gpu)
+    sceptr_gpu_kdt = KdTreeClusteringMachine(sceptr_gpu)
 
     stats = dict()
 
     for model in (
         levenshtein_bf,
         tcrdist_bf,
-        blastr_bf,
-        blastr_kdt,
+        sceptr_cpu_bf,
+        sceptr_gpu_bf,
+        sceptr_cpu_kdt,
+        sceptr_gpu_kdt
     ):
         model_stats = get_time_complexity_stats_for_model(model)
         stats[model.name] = model_stats
@@ -44,23 +55,20 @@ def get_time_complexity_stats_for_model(model: ClusteringMachine) -> dict:
     data = load_tcr_data()
     model_stats = dict()
 
-    for num_tcrs in (100, 500, 1_000, 5_000, 10_000, 50_000, 100_000):
+    for num_tcrs in np.logspace(1, 5, num=10, dtype=int):
         num_repeats = math.ceil(10_000 / num_tcrs)
 
         print(f"Benchmarking {num_tcrs} TCRs...")
-        if "KD Tree" not in model.name and num_tcrs > 10_000:
-            time_in_seconds = None
-        else:
-            repeated_times = timeit.repeat(lambda: model.cluster(data[:num_tcrs], 1), number=1, repeat=num_repeats)
-            time_in_seconds = statistics.mean(repeated_times)
+        repeated_times = timeit.repeat(lambda: model.cluster(data[:num_tcrs], 1), number=1, repeat=num_repeats)
+        time_in_seconds = statistics.mean(repeated_times)
         print(f"{time_in_seconds} seconds.")
-        model_stats[num_tcrs] = time_in_seconds
+        model_stats[int(num_tcrs)] = time_in_seconds
 
     return model_stats
 
 
 def load_tcr_data() -> DataFrame:
-    data = pd.read_csv("/home/yutanagano/UCLOneDrive/MBPhD/projects/tcr_embedder/data/preprocessed/olga/olga.csv")
+    data = pd.read_csv("tcr_data/preprocessed/olga/olga.csv")
     data.TRAV = data.TRAV.map(lambda x: x+"*01")
     data.TRBV = data.TRBV.map(lambda x: x+"*01")
     return data
