540097ac2a24f8e35e32761a3e711153f256d4ea
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-04-18 22:37:41 +0100

    Implement epitope background dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -40,3 +40,39 @@ class EpitopeBalancedTcrDataset(Dataset):
         tcr = tcrs_against_pmhc.sample().item()
 
         return TcrPmhcPair(tcr, pmhc)
+    
+
+class EpitopeBackgroundTcrDataset(Dataset):
+    def __init__(self, data: DataFrame, num_samples_per_pmhc_per_epoch: int):
+        super().__init__()
+        tcr_pmhc_series = schema.generate_tcr_pmhc_series(data)
+        tcr_pmhc_df = tcr_pmhc_series.to_frame().apply(
+            lambda row: (row.item().tcr, row.item().pmhc),
+            axis=1,
+            result_type="expand"
+        )
+        tcr_pmhc_df.columns = ["tcr", "pmhc"]
+        labelled_data_mask = tcr_pmhc_df.pmhc.map(lambda pmhc: pmhc.epitope_sequence is not None)
+
+        self._labelled_data = tcr_pmhc_df.loc[labelled_data_mask].reset_index(drop=True)
+        self._background_data = tcr_pmhc_df.loc[~labelled_data_mask].reset_index(drop=True)
+
+        self._pmhcs = self._labelled_data.pmhc.unique()
+        self._num_samples_per_specificity_per_epoch = num_samples_per_pmhc_per_epoch
+
+    def __len__(self) -> int:
+        return len(self._pmhcs) * self._num_samples_per_specificity_per_epoch * 2
+
+    def __getitem__(self, index: int) -> TcrPmhcPair:
+        index = index % len(self)
+        return_labelled_sequence = index % 2 == 0
+
+        if return_labelled_sequence:
+            pmhc = self._pmhcs[(index // 2) % len(self._pmhcs)]
+            tcrs_against_pmhc = self._labelled_data.tcr[self._labelled_data.pmhc == pmhc]
+            tcr = tcrs_against_pmhc.sample().item()
+            return TcrPmhcPair(tcr, pmhc)
+        
+        tcr = self._background_data.tcr.sample().item()
+        pmhc = schema.make_pmhc_from_components(None, None, None)
+        return TcrPmhcPair(tcr, pmhc)
--- tests/test_data/test_tcr_dataset.py
+++ tests/test_data/test_tcr_dataset.py
@@ -1,23 +1,75 @@
 import pytest
-from src.data.dataset import TcrDataset
+from src.data.dataset import TcrDataset, EpitopeBackgroundTcrDataset
 from libtcrlm import schema
 
 
-def test_len(tcr_dataset):
-    assert len(tcr_dataset) == 3
+class TestTcrDataset:
+    @pytest.fixture
+    def tcr_dataset(self, mock_data_df):
+        return TcrDataset(mock_data_df)
+
+    def test_len(self, tcr_dataset):
+        assert len(tcr_dataset) == 3
+
+    def test_getitem(self, tcr_dataset):
+        first_tcr_pmhc_pair = tcr_dataset[0]
+        expected_tcr = schema.make_tcr_from_components(
+            "TRAV1-1*01", "CAVKASGSRLT", "TRBV2*01", "CASSDRAQPQHF"
+        )
+        expected_pmhc = schema.make_pmhc_from_components("CLAMP", "HLA-A*01", "B2M")
+
+        assert first_tcr_pmhc_pair.tcr == expected_tcr
+        assert first_tcr_pmhc_pair.pmhc == expected_pmhc
 
 
-def test_getitem(tcr_dataset):
-    first_tcr_pmhc_pair = tcr_dataset[0]
-    expected_tcr = schema.make_tcr_from_components(
-        "TRAV1-1*01", "CAVKASGSRLT", "TRBV2*01", "CASSDRAQPQHF"
-    )
-    expected_pmhc = schema.make_pmhc_from_components("CLAMP", "HLA-A*01", "B2M")
+class TestEpitopeBackgroundDataset:
+    @pytest.fixture
+    def epitope_background_dataset(self, mock_data_df):
+        mock_data_df.loc[1,"Epitope"] = None
+        return EpitopeBackgroundTcrDataset(mock_data_df, 1)
 
-    assert first_tcr_pmhc_pair.tcr == expected_tcr
-    assert first_tcr_pmhc_pair.pmhc == expected_pmhc
+    def test_len(self, epitope_background_dataset):
+        assert len(epitope_background_dataset) == 4
 
+    def test_getitem(self, epitope_background_dataset):
+        first_tcr_pmhc_pair = epitope_background_dataset[0]
+        expected_tcr = schema.make_tcr_from_components(
+            "TRAV1-1*01", "CAVKASGSRLT", "TRBV2*01", "CASSDRAQPQHF"
+        )
+        expected_pmhc = schema.make_pmhc_from_components("CLAMP", "HLA-A*01", "B2M")
 
-@pytest.fixture
-def tcr_dataset(mock_data_df):
-    return TcrDataset(mock_data_df)
+        assert first_tcr_pmhc_pair.tcr == expected_tcr
+        assert first_tcr_pmhc_pair.pmhc == expected_pmhc
+
+        second_tcr_pmhc_pair = epitope_background_dataset[1]
+        expected_tcr = schema.make_tcr_from_components(
+            "TRAV1-1*01", "CLANGSRLT", "TRBV2*01", "CASSANDRAF"
+        )
+
+        assert second_tcr_pmhc_pair.tcr == expected_tcr
+        assert second_tcr_pmhc_pair.pmhc.epitope_sequence is None
+        assert second_tcr_pmhc_pair.pmhc.mhc_a.symbol is None
+        assert second_tcr_pmhc_pair.pmhc.mhc_b.symbol is None
+
+        third_tcr_pmhc_pair = epitope_background_dataset[2]
+        expected_tcr = schema.make_tcr_from_components(
+            "TRAV5*01", "CAVKASGSRLT", "TRBV6-9*01", "CASSDRAQPQHF"
+        )
+        expected_pmhc = schema.make_pmhc_from_components("STEAK", "HLA-A*02", "B2M")
+
+        assert third_tcr_pmhc_pair.tcr == expected_tcr
+        assert third_tcr_pmhc_pair.pmhc == expected_pmhc
+
+        expected_tcr = schema.make_tcr_from_components(
+            "TRAV1-1*01", "CLANGSRLT", "TRBV2*01", "CASSANDRAF"
+        )
+
+        fourth_tcr_pmhc_pair = epitope_background_dataset[3]
+        expected_tcr = schema.make_tcr_from_components(
+            "TRAV1-1*01", "CLANGSRLT", "TRBV2*01", "CASSANDRAF"
+        )
+
+        assert fourth_tcr_pmhc_pair.tcr == expected_tcr
+        assert fourth_tcr_pmhc_pair.pmhc.epitope_sequence is None
+        assert fourth_tcr_pmhc_pair.pmhc.mhc_a.symbol is None
+        assert fourth_tcr_pmhc_pair.pmhc.mhc_b.symbol is None
